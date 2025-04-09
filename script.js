4fa3f8818bf08a031ca062c2f0b43baed8cc4607
Stuart Race <stuart.race@bt.com>
2025-04-09 12:56:55 +0100

    Add level 4 swimming times

--- script.js
+++ script.js
@@ -100,7 +100,8 @@ export function loadData() {
   $("#recordedSwimmers").val(swimmerNumber);
 
   document.getElementById("times-table").innerHTML = "";
-  document.getElementById("recorded-times-cards").innerHTML = "";
+  $("#recorded-times-cards").empty();
+  $("#recorded-l4-times-cards").empty();
   document.getElementById("events-entered-cards").innerHTML = "";
 
   const swimmersEvents = results.events.filter((event) => event.results.some((result) => result[0] === swimmerNumber));
@@ -130,26 +131,50 @@ export function loadData() {
     return result;
   });
 
-  let swimmerMap = {};
+  let swimmerMapL3Plus = {};
 
-  for (const event of results.events) {
+  for (const event of results.events.filter((e) => e.level < 4)) {
     const swimmersResults = event.results.filter((result) => result[0] === swimmerNumber);
     for (const result of swimmersResults) {
       const distanceNum = result[5].split(" ")[0];
       const distance = result[5].split(" ")[0] + "m";
       const style = result[5].split(distanceNum + " ")[1];
-      if (!swimmerMap[distance]) {
-        swimmerMap[distance] = {};
+      if (!swimmerMapL3Plus[distance]) {
+        swimmerMapL3Plus[distance] = {};
       }
 
-      if (!swimmerMap[distance][style]) {
-        swimmerMap[distance][style] = {};
+      if (!swimmerMapL3Plus[distance][style]) {
+        swimmerMapL3Plus[distance][style] = {};
       }
 
-      if (!swimmerMap[distance][style].time || getTimeAsSeconds(result[7]) < getTimeAsSeconds(swimmerMap[distance][style].time)) {
-        swimmerMap[distance][style].time = result[7];
-        swimmerMap[distance][style].eventName = event.eventName;
-        swimmerMap[distance][style].date = event.date;
+      if (!swimmerMapL3Plus[distance][style].time || getTimeAsSeconds(result[7]) < getTimeAsSeconds(swimmerMapL3Plus[distance][style].time)) {
+        swimmerMapL3Plus[distance][style].time = result[7];
+        swimmerMapL3Plus[distance][style].eventName = event.eventName;
+        swimmerMapL3Plus[distance][style].date = event.date;
+      }
+    }
+  }
+
+  let swimmerMapL4 = {};
+
+  for (const event of results.events.filter((e) => e.level === "4")) {
+    const swimmersResults = event.results.filter((result) => result[0] === swimmerNumber);
+    for (const result of swimmersResults) {
+      const distanceNum = result[5].split(" ")[0];
+      const distance = result[5].split(" ")[0] + "m";
+      const style = result[5].split(distanceNum + " ")[1];
+      if (!swimmerMapL4[distance]) {
+        swimmerMapL4[distance] = {};
+      }
+
+      if (!swimmerMapL4[distance][style]) {
+        swimmerMapL4[distance][style] = {};
+      }
+
+      if (!swimmerMapL4[distance][style].time || getTimeAsSeconds(result[7]) < getTimeAsSeconds(swimmerMapL4[distance][style].time)) {
+        swimmerMapL4[distance][style].time = result[7];
+        swimmerMapL4[distance][style].eventName = event.eventName;
+        swimmerMapL4[distance][style].date = event.date;
       }
     }
   }
@@ -221,7 +246,7 @@ export function loadData() {
       let delta;
 
       try {
-        thisTime = swimmerMap[distance][type].time;
+        thisTime = swimmerMapL3Plus[distance][type].time;
       } catch (e) {
         // console.log("Nah", e)
       }
@@ -249,7 +274,7 @@ export function loadData() {
         }</div><div class="time-cell">${printableTime}</div> <div class="time-cell ${delta > 0 ? "negative-delta" : "positive-delta"}">${delta.toFixed(
           2
         )}</div></div>`;
-        cardHtml += `<div>At: ${swimmerMap[distance][type].eventName} on: ${swimmerMap[distance][type].date}</div>`;
+        cardHtml += `<div>At: ${swimmerMapL3Plus[distance][type].eventName} on: ${swimmerMapL3Plus[distance][type].date}</div>`;
         cardHtml += "</div>";
 
         document.getElementById("recorded-times-cards").innerHTML += cardHtml;
@@ -257,8 +282,55 @@ export function loadData() {
     }
   }
 
-  if (Object.keys(swimmerMap).length === 0) {
+  if (Object.keys(swimmerMapL4).length === 0) {
+    document.getElementById("recorded-l4-times-cards").innerHTML += "No swims found";
+  } else {
+    renderLevel4Times(swimmerMapL4, distances, recordedTypes, eventTimesTypeMap, category);
+  }
+
+  if (Object.keys(swimmerMapL3Plus).length === 0) {
     document.getElementById("recorded-times-cards").innerHTML += "No swims found";
+  }
+}
+
+function renderLevel4Times(swimmerMapL4, distances, recordedTypes, eventTimesTypeMap, category) {
+  // Render recorded times
+  for (const distance of distances) {
+    for (const type of recordedTypes) {
+      let thisTime = null;
+      let delta;
+
+      try {
+        thisTime = swimmerMapL4[distance][type].time;
+      } catch (e) {
+        // console.log("Nah", e)
+      }
+
+      if (thisTime !== null) {
+        const printableTime = thisTime;
+
+        if (thisTime.includes(":")) {
+          thisTime = parseFloat(thisTime.split(":")[0]) * 60 + parseFloat(thisTime.split(":")[1]);
+        }
+        try {
+          let countyTime = countyTimesData[category][distance][type][age];
+          if (countyTime?.includes(":")) {
+            countyTime = parseFloat(countyTime.split(":")[0]) * 60 + parseFloat(countyTime.split(":")[1]);
+          }
+          delta = Number.parseFloat(thisTime) - Number.parseFloat(countyTime);
+        } catch (err) {
+          console.log("err", err);
+        }
+
+        let cardHtml = "";
+        cardHtml += "<div class='event-card'>";
+        cardHtml += `<div class='event-card-top-row'><div class='event-title'>${distance} ${eventTimesTypeMap[type]}</div><div></div><div class="time-cell">${printableTime}</div></div>`;
+        cardHtml += `<div>At: ${swimmerMapL4[distance][type].eventName} on: ${swimmerMapL4[distance][type].date}</div>`;
+        cardHtml += "</div>";
+
+        document.getElementById("recorded-l4-times-cards").innerHTML += cardHtml;
+      }
+    }
   }
 }
 
@@ -277,24 +349,6 @@ function saveTime() {
   localStorage.setItem(`child1-event-${type}-${distance}`, timeEntry);
 
   loadData();
-}
-
-function getFastestTime(firstTime, secondTime) {
-  let time1 = getTimeAsSeconds(firstTime);
-  let time2 = getTimeAsSeconds(secondTime);
-  if (!time1?.length > 0) {
-    return time2;
-  }
-
-  if (!time2?.length > 0) {
-    return time1;
-  }
-
-  if (time1 < time2) {
-    return time1;
-  } else {
-    return time2;
-  }
 }
 
 function getTimeAsSeconds(time) {
